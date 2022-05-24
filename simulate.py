d0c3341b254d9505e0022fe05932f3424789681e
Jeff Kaufman <jeff@jefftk.com>
2022-05-23 21:30:49 -0400

    better simulation

--- simulate.py
+++ simulate.py
@@ -1,3 +1,5 @@
+import math
+
 # name -> [0.3-1, 1-3, 3-10]
 FILTERS={
   'MERV-11': [.20,  .65, .85],
@@ -9,37 +11,58 @@ FILTERS={
   'HEPA':    [.9997, 1,  1],
 }
 
+# https://www.jefftk.com/p/how-big-are-covid-particles
 VIRUS=[.20, .29, .51]
+
+# https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7579175/
+PARTICLE_HALF_LIVES = [
+  # 0.1-1, half life ~4hr
+  4*60,
+  # 1-3, half life ~20min
+  20,
+  # 3-10, half life ~5min
+  5]
+
+PARTICLE_DECAY_RATES=[math.log(2) / half_life
+                      for half_life in PARTICLE_HALF_LIVES]
 
 def proportion_removed(purifier_cfm, room_size_cf, filtration):
   return purifier_cfm/room_size_cf*filtration
 
-def simulate(room_size_cf=1150,
-             natural_ventilation_ach=2,
+def simulate(purifier_ach=5,
+             ventilation_ach=2,
              duration=60,
-             purifier_cfm=0,
              purifier_filter='MERV-14'):
-  natural_ventilation_cfm = room_size_cf / 60 * natural_ventilation_ach
-  
+  room_size_cf = 1000
+  purifier_cfm = room_size_cf / 60 * purifier_ach
+  ventilation_cfm = room_size_cf / 60 * ventilation_ach
+
+
   air = [0, 0, 0]
-  
+
+  print ("\t".join([
+    "timestamp", "0.3-1μm", "1-3μm", "3-10μm", "total"]))
+
   for timestep in range(duration):
     for particle_size in range(len(air)):
       # infected person exhales
       air[particle_size] += VIRUS[particle_size]
-      
+
       # natural ventilation operates
       air[particle_size] *= 1 - proportion_removed(
-        natural_ventilation_cfm, room_size_cf, 1.0)
-          
+        ventilation_cfm, room_size_cf, 1.0)
+
       # purifier operates
       air[particle_size] *= 1 - proportion_removed(
         purifier_cfm, room_size_cf, FILTERS[purifier_filter][particle_size])
+
+      # particles settle
+      air[particle_size] *= 1 - PARTICLE_DECAY_RATES[particle_size]
 
     print("%s\t%s\t%.2f" % (
       timestep,
       "\t".join("%.2f" % x for x in air),
       sum(air)))
-    
+
 if __name__ == "__main__":
   simulate()
